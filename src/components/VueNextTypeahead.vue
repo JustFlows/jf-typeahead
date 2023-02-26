76ad36ace2a1f6af319cf731a3b135e7eab5a23f
Jay Doerga <doerga1@gmail.com>
2023-02-26 17:09:26 +0100

    remove test

--- src/components/VueNextTypeahead.vue
+++ src/components/VueNextTypeahead.vue
@@ -23,6 +23,7 @@
         :placeholder="placeholder"
         :value="value"
         @input="changeInput($event)"
+        @paste="changeInput($event)"
         @focus="onFocusin()"
         @blur="onFocusout()" />
       <i class="ico-search" v-if="!searching && !loadingParentComponent" />
